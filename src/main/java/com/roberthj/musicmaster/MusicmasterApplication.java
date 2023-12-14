7e30feb462bb048a40e5ead6a5bc4c84e0efc877
“Robert <roberthj@spotify.com>
2023-12-14 10:43:35 +0100

    Initial commit

---
+++ src/main/java/com/roberthj/musicmaster/MusicmasterApplication.java
@@ -0,0 +1,13 @@
+package com.roberthj.musicmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roberthj/musicmaster/MusicmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberthj.musicmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
